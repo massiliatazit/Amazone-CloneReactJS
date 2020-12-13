bd6e18dd26388fb356dae617e5726d81a42bd210
massiliatazit <massiliatazit@users.noreply.github.com>
2020-12-13 12:18:01 +0100

    .

--- amazone_clone/src/components/Home.jsx
+++ amazone_clone/src/components/Home.jsx
@@ -2,35 +2,33 @@ import React from "react";
 
 import "./home.css";
 
-import { Container, Jumbotron, Button, Row} from "react-bootstrap";
+import { Container, Jumbotron, Button, Row } from "react-bootstrap";
 import AddProduct from "./AddProduct.jsx";
 import Product from "./Product";
 
 function Home() {
   return (
     <Container>
-    <div className="home">
-      <Container fluid className=" px-0">
-        <img
-          className="home_image"
-          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2020/BlackFriday/Fuji_TallHero_BFWeek_v1_en_US_1x._CB415746974_.jpg"
-          style={{ height: "600px", width: "100%" }}
-          alt="home image"
-        ></img>
+      <div className="home">
+        <Container fluid className=" px-0">
+          <img
+            className="home_image"
+            src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2020/BlackFriday/Fuji_TallHero_BFWeek_v1_en_US_1x._CB415746974_.jpg"
+            style={{ height: "600px", width: "100%" }}
+            alt="home image"
+          ></img>
 
-        <Row>
+          <Row>
+            <AddProduct />
 
-        <AddProduct />
-        
-        <div className="d-flex justify-content-around">
+            <Row className="d-flex justify-content-around">
+              <Product />
+              <Product />
+              <Product />
 
-          <Product />
-          <Product />
-          <Product />
-
-          <Product />
-        </Row>
-        {/* <div className="home_row">
+              <Product />
+            </Row>
+            {/* <div className="home_row">
 
          
         </div>
@@ -43,12 +41,11 @@ function Home() {
  GetProducts/Frontend
           <Product />
         </div> */}
+          </div>
 
-        </div>
-
-        <div className="home_row"></div>
-      </Container>
-    </div>
+          <div className="home_row"></div>
+        </Container>
+      </div>
     </Container>
   );
 }
